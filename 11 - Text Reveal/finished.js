3fc5464f6d43f654ca1fad26594c59fb67a02700
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-04-23 17:25:51 +0700

    11 - Text Reveal ALL DONE

--- 11 - Text Reveal/finished.js
+++ 11 - Text Reveal/finished.js
@@ -1,22 +1,22 @@
 (() => {
   function onScroll() {
-    const sectionElems = Array.from(document.querySelectorAll('section'));
+    const sectionElems = Array.from(document.querySelectorAll('section')); // ดึงออกมาไม่ใช่ array มันดึงมาเป็น nodelist เลยครอบด้วย array.from
 
-    sectionElems.forEach(sectionElem => {
-      const imgElem = sectionElem.querySelector('img');
-      const textElem = sectionElem.querySelector('.text');
+    sectionElems.forEach(sectionElem => { //argument sectionElems แต่ละอัน
+      const imgElem = sectionElem.querySelector('img'); // ดึง img ออกมา
+      const textElem = sectionElem.querySelector('.text'); // ดึง class text ออกมา
 
-      const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10;
-      const scrollPosition = window.pageYOffset;
+      const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10;  //เมื่อเรา scroll ไป 1/10 ของ img แล้ว จะเริ่มแสดง text //ตำแหน่งที่แสดง text คือ ค่าสูงสุดของรูปเรา imageElement.offsetTop บวกกับความสูงของรูปหารด้วย 10 
+      const scrollPosition = window.pageYOffset; //เราจะดูว่าเลื่อนจอไปเท่าไหร่ (scroll ไปเท่าไหร่)
 
-      if (scrollPosition >= revealPosition) {
+      if (scrollPosition >= revealPosition) { //าเรา scroll ไปแล้วมากกว่าหรือเท่ากับ revealPosition ก็ให้แสดง text ขึ้นมา โดยให้ textElement เพิ่ม class reveal เข้าไป  //เริ่ม text opacity 0 แต่ reveral opa = 1 
         textElem.classList.add('reveal');
       }
     });
   }
 
   function run() {
-    document.addEventListener('scroll', onScroll);
+    document.addEventListener('scroll', onScroll); //เหมือนเดิม เมื่อ scroll ให้้เรียกใช้ฟังก์ชั่น onScroll
   }
 
   run();
