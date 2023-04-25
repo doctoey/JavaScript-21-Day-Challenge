4b4bf5db5e990e8e9a8b91cc77be7f3db6426da1
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-04-25 23:36:50 +0700

    14 - Carousel ALL DONE

--- 14 -  Carousel/start.js
+++ 14 -  Carousel/start.js
@@ -5,25 +5,25 @@
   function displayImage(imageElements, newIndex) {
     const lastIndex = imageElements.length - 1;
 
-    if (newIndex < 0) {
+    if (newIndex < 0) { //เพิ่ม condition ว่า ถ้าได้ index มาน้อยกว่า 0 แล้ว ให้แสดงรูปสุดท้ายในชุดนี้
       newIndex = lastIndex;
-    } else if (newIndex > lastIndex) {
+    } else if (newIndex > lastIndex) { //ถ้ารูปสุดท้ายหละ ก็เหมือนกัน index เกิน ให้แสดงรูปแรก เหมือนเดิม (index = 0)
       newIndex = 0;
     }
 
-    const newImage = imageElements[newIndex];
-    newImage.scrollIntoView({ behavior: 'smooth'});
+    const newImage = imageElements[newIndex]; //ดึงรูปปัจจุบันมาก่อน
+    newImage.scrollIntoView({ behavior: 'smooth'}); //เลื่อนรูปนี้มาที่หน้าจอเรา
 
-    currentIndex = newIndex
+    currentIndex = newIndex //อย่าลืม update currentIndex ด้วย หลังเลื่อนรูป
   }
 
   function run() {
-    const imageElements = document.querySelectorAll('img');
-    const previousButtonElement = document.querySelector('.previous');
-    const nextButtonElement = document.querySelector('.next');
+    const imageElements = document.querySelectorAll('img'); //ดึงรูปทั้งหมดออกมา 4 รูป
+    const previousButtonElement = document.querySelector('.previous'); //ปุ่มกลับ
+    const nextButtonElement = document.querySelector('.next'); //ปุ่มไป
 
-    previousButtonElement.addEventListener('click', () => displayImage(imageElements, currentIndex-1));
-    nextButtonElement.addEventListener('click', () => displayImage(imageElements, currentIndex+1));
+    previousButtonElement.addEventListener('click', () => displayImage(imageElements, currentIndex - 1)); // แตก index เริ่ม 0 แล้วไป -1 แตกกกกก
+    nextButtonElement.addEventListener('click', () => displayImage(imageElements, currentIndex + 1));
   }
 
   run();
