8adff0af098d987c52fd7aaeed5f85cc36e48cba
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-05-02 23:35:10 +0700

    18 - What is prototype ALL DONE

--- 18 - What is prototype/start.js
+++ 18 - What is prototype/start.js
@@ -2,7 +2,30 @@
   // เริ่มเขียนโค้ด
 
   // 1. Class vs Prototype 
+  class Person {
+
+  }
+  console.log(new Person()); //Person {}
+
   // 2. What's prototype?
-  // 3. Prototype chain
-  // 4. Extend a prototype
+  const name1 = 'doctoey';
+  console.log(name1.__proto__); //string prototype
+
+  const arr = [];
+  console.log(arr.__proto__); //method Array(0) find, forEach, map
+
+  // // 3. Prototype chain
+  const name2 = 'Cheewathun';
+  console.log(name2.toString()); //Cheewathun
+  console.log(name2.toLocaleString()); //Cheewathun
+  console.log(name2.__proto__); //string prototype
+  console.log(name2.__proto__.__proto__); //proto class object
+
+  // // 4. Extend a prototype
+  const name3 = 'Wahahaha';
+  function sayHello(val) {
+    console.log(`Hello ${val}`);
+  }
+  String.prototype.sayHello = sayHello; //[Object: null prototype] {}
+  name3.sayHello('World'); //Hello World
 })();
